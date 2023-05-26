ff165465f39f868a30da3f9970bd3929a4a26a53
kakwa <carpentier.pf@gmail.com>
2023-05-26 20:13:02 +0200

    basic bootstrap stuff for the templates

--- templates/page.go
+++ templates/page.go
@@ -5,56 +5,76 @@ var PageTpl = `
 <!DOCTYPE html>
 <html>
     {{- template "header" . }}
-<style>
-	.box { 
-	    justify-content:center;
-            display:flex;
-	    flex-wrap:wrap
-        }
 
-        /* Set up grid container */
-        .container {
-            display: grid;
-            grid-template-rows: auto 1fr auto; /* Header, Content, Footer */
-            min-height: 100vh;
-        }
+<body class="p-1 m-0 border-0 bd-example">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
 
-        /* Header styles */
-        .header {
-            background-color: #f5f5f5;;
-            padding: 20px;
-        }
+      <div class="container-xl">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
 
-        /* Content styles */
-        .content {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            padding: 20px;
-        }
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Servers</a>
 
-        /* Footer styles */
-        .footer {
-            background-color: #f5f5f5;;
-            padding: 20px;
-        }
-
-</style>
-<body>
-    <div class="container">
-        <header class="header">
-	    <a href="https://eu.wows-stats.kakwalab.ovh" target="_blank">EU server stats</a> | <a href="https://na.wows-stats.kakwalab.ovh" target="_blank">NA server stats</a> | <a href="https://asia.wows-stats.kakwalab.ovh" target="_blank">ASIA server stats</a>
-        </header>
-
-        <div class="content">
-	    <div class="box"> {{- range .Charts }} {{ template "base" . }} {{- end }} </div>
+              <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="https://asia.wows-stats.kakwalab.ovh">ASIA</a></li>
+                  <li><a class="dropdown-item" href="https://eu.wows-stats.kakwalab.ovh">EU</a></li>
+                  <li><a class="dropdown-item" href="https://na.wows-stats.kakwalab.ovh">NA</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/archive/">Older Reports</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="about">
+            <button type="button" class="btn btn-sm btn-outline-secondary ms-auto" data-bs-toggle="modal" data-bs-target="#aboutModal">
+                About
+            </button>
+          </form>
         </div>
+      </div>
+    </nav>
 
-        <footer class="footer">
-		<p>© wows-stat. All rights reserved | © Wargaming.net. All rights reserved | This is application is a third-party tool under Wargaming Developers Program.</p>
-        </footer>
+    <div class="container">
+      <div class="box"> {{- range .Charts }} {{ template "base" . }} {{- end }} </div>
     </div>
+  
+  
+  <!-- About modal -->
+  <div class="modal fade" id="aboutModal" tabindex="-1" aria-labelledby="aboutModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="aboutModalLabel">About this application</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+  		<p>© wows-stat. All rights reserved,  
+		<a class="icon-link icon-link-hover" style="--bs-icon-link-transform: translate3d(0, -.125rem, 0);" href="https://github.com/wows-tools/wows-stats">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-code-square" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                    <path d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z"/>
+                  </svg>
+		  source code
+		</a>.
+		</p>
+  		<p>© Wargaming.net. All rights reserved.</p>
+  		<p>This application is a third-party tool under Wargaming Developers Program,
+                  <a class="icon-link icon-link-hover" style="--bs-icon-link-transform: translate3d(0, -.125rem, 0);" href="https://worldofwarships.com">
+		    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tsunami" viewBox="0 0 16 16">
+		      <path d="M.036 12.314a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.757-.703a.5.5 0 0 1-.278-.65zm0 2a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.757-.703a.5.5 0 0 1-.278-.65zM2.662 8.08c-.456 1.063-.994 2.098-1.842 2.804a.5.5 0 0 1-.64-.768c.652-.544 1.114-1.384 1.564-2.43.14-.328.281-.68.427-1.044.302-.754.624-1.559 1.01-2.308C3.763 3.2 4.528 2.105 5.7 1.299 6.877.49 8.418 0 10.5 0c1.463 0 2.511.4 3.179 1.058.67.66.893 1.518.819 2.302-.074.771-.441 1.516-1.02 1.965a1.878 1.878 0 0 1-1.904.27c-.65.642-.907 1.679-.71 2.614C11.076 9.215 11.784 10 13 10h2.5a.5.5 0 0 1 0 1H13c-1.784 0-2.826-1.215-3.114-2.585-.232-1.1.005-2.373.758-3.284L10.5 5.06l-.777.388a.5.5 0 0 1-.447 0l-1-.5a.5.5 0 0 1 .447-.894l.777.388.776-.388a.5.5 0 0 1 .447 0l1 .5a.493.493 0 0 1 .034.018c.44.264.81.195 1.108-.036.328-.255.586-.729.637-1.27.05-.529-.1-1.076-.525-1.495-.426-.42-1.19-.77-2.477-.77-1.918 0-3.252.448-4.232 1.123C5.283 2.8 4.61 3.738 4.07 4.79c-.365.71-.655 1.433-.945 2.16-.15.376-.301.753-.463 1.13z"/>
+		    </svg>
+		    World of Warships Official Website
+                  </a>.
+                </p>
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              </div>
+          </div>
+      </div>
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
 {{ end }}
